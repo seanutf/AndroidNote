032144696a9cbd3c4e98511f341d7b42b387ac2f
sean <seanutf@gmail.com>
2017-06-07 22:49:59 +0800

    增加toolbar

--- app/src/main/java/com/sean/zq/android/view/widget/toolbar/ToolBarActivity.java
+++ app/src/main/java/com/sean/zq/android/view/widget/toolbar/ToolBarActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.sean.note.android.R;
 
 /**
- * Created by sks on 2017/6/6.
+ * Created by sean on 2017/6/6.
  */
 
 public class ToolBarActivity extends AppCompatActivity {
@@ -18,5 +20,30 @@ public class ToolBarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_layout_widget_toolbar);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu_toolbar_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
