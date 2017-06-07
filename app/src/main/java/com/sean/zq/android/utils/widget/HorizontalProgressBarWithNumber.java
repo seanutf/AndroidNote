420265064c509b5698f0c4f2eee4b9364488f7dd
yi.zhang <seanutf@gmail.com>
2017-06-07 15:16:37 +0800

    增加progressbar

--- app/src/main/java/com/sean/zq/android/utils/widget/HorizontalProgressBarWithNumber.java
+++ app/src/main/java/com/sean/zq/android/utils/widget/HorizontalProgressBarWithNumber.java
@@ -2,7 +2,6 @@ package com.sean.zq.android.utils.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.TypedValue;
@@ -11,14 +10,10 @@ import android.widget.ProgressBar;
 import com.sean.note.android.R;
 
 /**
- * Created by sean on 2017/6/6.
- * 源于网络：http://blog.csdn.net/lmj623565791/article/details/43371299
- * 做了些小改动，去掉了百分比的显示
- * 原版见{@link HorizontalProgressBarWithNumber}
+ * Created by sks on 2017/6/7.
  */
 
-public class HorizontalProgressBar extends ProgressBar {
-
+public class HorizontalProgressBarWithNumber extends ProgressBar {
 
     private static final int DEFAULT_TEXT_SIZE = 10;
     private static final int DEFAULT_TEXT_COLOR = 0XFFFC00D1;
@@ -38,7 +33,7 @@ public class HorizontalProgressBar extends ProgressBar {
     /**
      * size of text (sp)
      */
-    protected int mTextSize = dp2px(DEFAULT_TEXT_SIZE);
+    protected int mTextSize = sp2px(DEFAULT_TEXT_SIZE);
 
     /**
      * offset of draw progress
@@ -71,11 +66,12 @@ public class HorizontalProgressBar extends ProgressBar {
 
     protected static final int VISIBLE = 0;
 
-    public HorizontalProgressBar(Context context, AttributeSet attrs) {
+    public HorizontalProgressBarWithNumber(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public HorizontalProgressBar(Context context, AttributeSet attrs, int defStyle) {
+    public HorizontalProgressBarWithNumber(Context context, AttributeSet attrs,
+                                           int defStyle) {
         super(context, attrs, defStyle);
 
         setHorizontalScrollBarEnabled(true);
@@ -95,90 +91,43 @@ public class HorizontalProgressBar extends ProgressBar {
     private void obtainStyledAttributes(AttributeSet attrs) {
         // init values from custom attributes
         final TypedArray attributes = getContext().obtainStyledAttributes(
-                attrs, R.styleable.HorizontalProgressBar);
+                attrs, R.styleable.HorizontalProgressBarWithNumber);
 
         mTextColor = attributes
                 .getColor(
-                        R.styleable.HorizontalProgressBar_progress_text_color,
+                        R.styleable.HorizontalProgressBarWithNumber_progress_text_color,
                         DEFAULT_TEXT_COLOR);
         mTextSize = (int) attributes.getDimension(
-                R.styleable.HorizontalProgressBar_progress_text_size,
+                R.styleable.HorizontalProgressBarWithNumber_progress_text_size,
                 mTextSize);
 
         mReachedBarColor = attributes
                 .getColor(
-                        R.styleable.HorizontalProgressBar_progress_reached_color,
+                        R.styleable.HorizontalProgressBarWithNumber_progress_reached_color,
                         mTextColor);
         mUnReachedBarColor = attributes
                 .getColor(
-                        R.styleable.HorizontalProgressBar_progress_unreached_color,
+                        R.styleable.HorizontalProgressBarWithNumber_progress_unreached_color,
                         DEFAULT_COLOR_UNREACHED_COLOR);
         mReachedProgressBarHeight = (int) attributes
                 .getDimension(
-                        R.styleable.HorizontalProgressBar_progress_reached_bar_height,
+                        R.styleable.HorizontalProgressBarWithNumber_progress_reached_bar_height,
                         mReachedProgressBarHeight);
         mUnReachedProgressBarHeight = (int) attributes
                 .getDimension(
-                        R.styleable.HorizontalProgressBar_progress_unreached_bar_height,
+                        R.styleable.HorizontalProgressBarWithNumber_progress_unreached_bar_height,
                         mUnReachedProgressBarHeight);
         mTextOffset = (int) attributes
                 .getDimension(
-                        R.styleable.HorizontalProgressBar_progress_text_offset,
+                        R.styleable.HorizontalProgressBarWithNumber_progress_text_offset,
                         mTextOffset);
 
-        int textVisible = attributes
-                .getInt(R.styleable.HorizontalProgressBar_progress_text_visibility,
-                        VISIBLE);
+        int textVisible = attributes.getInt(R.styleable.HorizontalProgressBarWithNumber_progress_text_visibility,
+                VISIBLE);
         if (textVisible != VISIBLE) {
             mIfDrawText = false;
         }
         attributes.recycle();
-    }
-
-    @Override
-    protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-
-        if (heightMode != MeasureSpec.EXACTLY) {
-
-            float textHeight = (mPaint.descent() + mPaint.ascent());
-            int exceptHeight = (int) (getPaddingTop() + getPaddingBottom() + Math
-                    .max(Math.max(mReachedProgressBarHeight,
-                            mUnReachedProgressBarHeight), Math.abs(textHeight)));
-
-            heightMeasureSpec = MeasureSpec.makeMeasureSpec(exceptHeight,
-                    MeasureSpec.EXACTLY);
-        }
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
-    }
-
-    @Override
-    protected synchronized void onDraw(Canvas canvas) {
-        canvas.save();
-        //画笔平移到指定paddingLeft， getHeight() / 2位置，注意以后坐标都为以此为0，0
-        canvas.translate(getPaddingLeft(), getHeight() / 2);
-
-        //当前进度和总值的比例
-        float radio = getProgress() * 1.0f / getMax();
-        //已到达的宽度
-        float progressPosX = (int) (mRealWidth * radio);
-
-        // 绘制已到达的进度
-        float endX = progressPosX - mTextOffset / 2;
-        if (endX > 0) {
-            mPaint.setColor(mReachedBarColor);
-            mPaint.setStrokeWidth(mReachedProgressBarHeight);
-            canvas.drawLine(0, 0, endX, 0, mPaint);
-        }
-        canvas.restore();
-    }
-
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        mRealWidth = w - getPaddingRight() - getPaddingLeft();
-
     }
 
     /**
