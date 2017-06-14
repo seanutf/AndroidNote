dcc5d13ce53d6e988fc01143b9077553142ab107
yi.zhang <seanutf@gmail.com>
2017-06-14 15:05:30 +0800

    增加ToolBar常用处理

--- app/src/main/java/com/sean/zq/android/view/widget/toolbar/ToolBarActivity.java
+++ app/src/main/java/com/sean/zq/android/view/widget/toolbar/ToolBarActivity.java
@@ -2,7 +2,10 @@ package com.sean.zq.android.view.widget.toolbar;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,12 +22,50 @@ public class ToolBarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout_widget_toolbar);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setLogo(R.mipmap.ic_launcher);
+        myToolbar.setTitle("主标题");
+        myToolbar.setSubtitle("副标题");
         setSupportActionBar(myToolbar);
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu_toolbar_activity, menu);
+        // ...
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView =
+                (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        // Configure the search info and add any event listeners...
+
+        // Define the listener
+        MenuItemCompat.OnActionExpandListener expandListener = new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                // Do something when action item collapses
+                return true;  // Return true to collapse action view
+            }
+
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                // Do something when expanded
+                return true;  // Return true to expand action view
+            }
+        };
+
+        // Get the MenuItem for the action item
+        MenuItem actionMenuItem = menu.findItem(R.id.action_search);
+
+        // Assign the listener to that action item
+        MenuItemCompat.setOnActionExpandListener(actionMenuItem, expandListener);
+
+        // Any other things you have to do when creating the options menu…
+
         return true;
     }
 
@@ -38,6 +79,12 @@ public class ToolBarActivity extends AppCompatActivity {
             case R.id.action_favorite:
                 // User chose the "Favorite" action, mark the current item
                 // as a favorite...
+                return true;
+            case R.id.action_search:
+
+                return true;
+            case R.id.action_share:
+
                 return true;
 
             default:
